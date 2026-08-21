bad4673b0919c68fee34c042ed968219bcb56838
Vladimir Matveenko <matvej83@gmail.com>
2026-08-21 16:47:42 +0300

    tests changed

--- test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:chatting_app/app/my_app.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-  });
-}
