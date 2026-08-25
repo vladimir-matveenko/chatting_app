ecbc3fe659a44be1bd2bb98a7a98751efcafdf64
vladimir-matveenko <55102679+vladimir-matveenko@users.noreply.github.com>
2026-08-25 18:10:34 +0000

    Deploying to gh-pages from @ vladimir-matveenko/chatting_app@fdee08bc4237f7ca7e9ee1b27a05a984dc345c46 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1264251632" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "406352810" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
